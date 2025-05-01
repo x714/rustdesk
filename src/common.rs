02d252c4d0a59c4510e9fa3ef71246eca53b233c
x714 <714193127@qq.com>
2025-05-01 21:27:33 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -956,7 +956,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://113.45.43.99".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
